09fe832690a6d32fb188abccf3450aace1737883
Beny <qkdehdgur3@gmail.com>
2025-04-14 15:18:22 +0900

    250408 문제 풀이 완료

--- src/April/first/CountPnY.java
+++ src/April/first/CountPnY.java
@@ -1,4 +1,14 @@
 package April.first;
 
 public class CountPnY {
+    boolean solution(String s) {
+        long p = s.toLowerCase().chars().filter(c -> c == 'p').count();
+        long y = s.toLowerCase().chars().filter(c -> c == 'y').count();
+
+        boolean answer = p == y;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+        return answer;
+    }
 }
