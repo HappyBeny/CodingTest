b2ae02cc87a7846a25b943d1c41df4045ee88889
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-04-21 17:02:11 +0900

    ⚡ : 250421 1번 문제풀이 완료

--- src/april/third/BudgetSupport.java
+++ src/april/third/BudgetSupport.java
@@ -1,4 +1,20 @@
 package april.third;
 
+import java.util.Arrays;
+
 public class BudgetSupport {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int answer = 0;
+        int sum = 0;
+
+        for (int i = 0; i < d.length; i++) {
+            sum += d[i];
+            if (budget - sum < 0) {
+                break;
+            }
+            answer++;
+        }
+        return answer;
+    }
 }
