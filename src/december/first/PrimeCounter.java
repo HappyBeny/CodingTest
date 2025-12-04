c8351eb81c7b410cb701043660bbe8de1e8fbcd9
Beny <qkdehdgur3@gmail.com>
2025-12-04 12:22:11 +0900

    251204 소수만들기 문제 해결

--- src/december/first/PrimeCounter.java
+++ src/december/first/PrimeCounter.java
@@ -2,11 +2,30 @@ package december.first;
 
 public class PrimeCounter {
     public int solution(int[] nums) {
-        int answer = -1;
+        int answer = 0;
+        int size = nums.length;
+        int checkNum;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+        for (int i = 0; i < size-2; i++) {
+            for (int j = i+1; j < size-1; j++) {
+                for (int k = j+1; k < size; k++) {
 
+                    boolean isPrime = true;
+                    checkNum = nums[i] + nums[j] + nums[k];
+
+                    for (int l = 2; l <= Math.sqrt(checkNum); l++) {
+                        if (checkNum % l == 0) {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+
+                    if (isPrime) {
+                        answer++;
+                    }
+                }
+            }
+        }
         return answer;
     }
 }
